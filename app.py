6805cc8eddf75360836d86443f572fc5260ad40e
Nareshgopi <101465841+Nareshgopi@users.noreply.github.com>
2025-04-12 23:16:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,13 +16,13 @@ ln = "languages"
 defaultlanguage = "english"
 @app.route("/")
 def index():
-  return render_template("templates/index.html",languages=languages,welcomemessage=welcomemessage,data=data,services=services,name=name,email=email,phonenumber=phonenumber,ln=ln,defaultlanguage=defaultlanguage)
+  return render_template("index.html",languages=languages,welcomemessage=welcomemessage,data=data,services=services,name=name,email=email,phonenumber=phonenumber,ln=ln,defaultlanguage=defaultlanguage)
 @app.route("/<string:languageselected>")
 def languageselected():
   welcomemessage = translator.translate(welcomemessage,src=auto,dest=[languageselected])
   data = translator.translate(data,src=auto,dest=[languageselected])
   ln = translator.translate(ln,src=auto,dest=[languageselected])
   defaultlanguage = languageselected
-  return render_template("templates/index.html",languages=languages,welcomemessage=welcomemessage,data=data,services=services,name=name,email=email,phonenumber=phonenumber,ln=ln,defaultlanguage=defaultlanguage)
+  return render_template("index.html",languages=languages,welcomemessage=welcomemessage,data=data,services=services,name=name,email=email,phonenumber=phonenumber,ln=ln,defaultlanguage=defaultlanguage)
 if __name__ == "__main__":
   app.run(debug=True)
