71453256dc6fcdbeb7b07e7b0f8aff56891bc661
Nareshgopi <101465841+Nareshgopi@users.noreply.github.com>
2025-04-12 15:29:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask,render_template,request
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///nareshgopiproject1.db"
 if __name__ == "__main__":
   app.run(debug=True)
