912c1a583692f0bea4a1bec6bdd417baf0d3472e
Nareshgopi <101465841+Nareshgopi@users.noreply.github.com>
2025-04-12 22:46:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ name = "kurra naresh gopi"
 email = "nareshgopiyadavkurra@gmail.com"
 phonenumber = "9390186116"
 translator = Translator()
-languages = [translator.translate("telugu",src=auto,dest=["telugu"]),translator.translate("hindi",src=auto,dest=["hindi"]),translator.translate("english",src=auto,dest=["english"])]
+languages = [translator.translate("telugu",src="auto",dest=["telugu"]),translator.translate("hindi",src="auto",dest=["hindi"]),translator.translate("english",src="auto",dest=["english"])]
 welcomemessage = "hi welcome to software services"
 data = "in software services we are providing software installations like coding software or normal software, we can do it in offline or remote, job applications are government appliactions and it applications, msoffice like excel sheet, power point presentations, documentations, web development front end and back end, this is about software services"
 services = ["software installations","msoffice(excel sheet, power point presentation, documentation)", "web development (front end back end)", "job applications(government it)"]
