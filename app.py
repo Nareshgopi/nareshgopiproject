bb79b8262fc49985744a4ea743b555099b32626c
Nareshgopi <101465841+Nareshgopi@users.noreply.github.com>
2025-04-12 20:02:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,15 @@ languages = [translator.translate("telugu",src=auto,dest=["telugu"]),translator.
 welcomemessage = "hi welcome to software services"
 data = "in software services we are providing software installations like coding software or normal software, we can do it in offline or remote, job applications are government appliactions and it applications, msoffice like excel sheet, power point presentations, documentations, web development front end and back end, this is about software services"
 services = ["software installations","msoffice(excel sheet, power point presentation, documentation)", "web development (front end back end)", "job applications(government it)"]
+ln = "languages"
+defaultlanguage = "english"
 app.route("/")
 def index():
-  return render_template("index.html")
+  return render_template("index.html",languages=languages,welcomemessage=welcomemessage,data=data,services=services,name=name,email=email,phonenumber=phonenumber,ln=ln,defaultlanguage=defaultlanguage)
+app.route("/<string:languageselected>")
+def languageselected():
+  welcomemessage = translator.translate(welcomemessage,src=auto,dest=[languageselected])
+  data = translator.translate(data,src=auto,dest=[languageselected])
+  ln = translator.translate(ln,src=auto,dest=[languageselected])
 if __name__ == "__main__":
   app.run(debug=True)
