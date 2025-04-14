c4f926634babf29e88c63b050aa0f3fa1e93412b
Nareshgopi <101465841+Nareshgopi@users.noreply.github.com>
2025-04-14 14:08:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ phonenumber = "9390186116"
 translator = Translator()
 languages = [translator.translate("telugu",src="auto",dest=["telugu"]),translator.translate("hindi",src="auto",dest=["hindi"]),translator.translate("english",src="auto",dest=["english"])]
 welcomemessage = "hi welcome to software services"
-data = "in software services we are providing software installations like coding software or normal software, we can do it in offline or remote, job applications are government appliactions and it applications, msoffice like excel sheet, power point presentations, documentations, web development front end and back end, this is about software services"
-services = ["software installations","msoffice(excel sheet, power point presentation, documentation)", "web development (front end back end)", "job applications(government it)"]
+data = "in software services we are providing software installations like coding software or normal software, we can do it in offline or remote, web development front end and back end, this is about software services"
+services = ["software installations", "web development (front end back end)"]
 ln = "languages"
 defaultlanguage = "english"
 @app.route("/")
